ae876d3793a14f14f4513b1fea91e6ce4e6ef365
Yusuke DOI <doi@wide.ad.jp>
2014-03-08 04:50:01 +0900

    u_int8_t instead of char

--- src/encoding.c
+++ src/encoding.c
@@ -34,7 +34,7 @@ int encmax(int b){
 // prefix_len = leading number of bits for the first octet
 // returns number of bytes consumed
 // section 4.1.1
-int enc_integer(char *buf, int buf_len, int64_t value, int prefix_len){
+int enc_integer(u_int8_t *buf, int buf_len, int64_t value, int prefix_len){
   int max_first = encmax(8-prefix_len);
   int count = 0;
   if (value < max_first){
@@ -58,7 +58,7 @@ int enc_integer(char *buf, int buf_len, int64_t value, int prefix_len){
 
 // next_read_pp is pointer-pointer to return where to read next
 // section 4.1.1
-int64_t dec_integer(char *data, int data_len, int prefix_len, char **next_read_pp){
+int64_t dec_integer(u_int8_t *data, int data_len, int prefix_len, u_int8_t **next_read_pp){
   int64_t value;
   long m;
   int max_first = encmax(8-prefix_len);
@@ -84,9 +84,9 @@ int64_t dec_integer(char *data, int data_len, int prefix_len, char **next_read_p
 }
 
 // section 4.1.2
-int enc_string(char *buf, int buf_len, char *s, int s_len){
+int enc_string(u_int8_t *buf, int buf_len, u_int8_t *s, int s_len){
   int i;
-  char *wp;
+  u_int8_t *wp;
   buf[0] = 0;
   if ((i = enc_integer(buf, buf_len, s_len, 1)) < 0){
     return -1;
@@ -103,9 +103,9 @@ int enc_string(char *buf, int buf_len, char *s, int s_len){
 // buffer will have trailing \0, but it returns string length (\0 excluded)
 // section 4.1.2
 // FIXME: should it be zero-copy?
-int dec_string(char *data, int data_len, char *buf, int buf_len){
+int dec_string(u_int8_t *data, int data_len, u_int8_t *buf, int buf_len){
   int64_t slen;
-  char *s;
+  u_int8_t *s;
   if (data[0] & 0x80 == 0x80){
     fprintf(stderr, "dec_string: huffman encoding is not supported\n");
     return -1;
@@ -125,9 +125,9 @@ int dec_string(char *data, int data_len, char *buf, int buf_len){
 
 int main(int argc, char **argv){
 
-  char buf[128];
+  u_int8_t buf[128];
   int buflen = 128;
-  char *rp;
+  u_int8_t *rp;
   int r;
   int64_t v;
   int i;
@@ -149,8 +149,8 @@ int main(int argc, char **argv){
   r = enc_integer(buf, sizeof(buf), 1337, 3);
   assert(r == 3);
   assert(buf[0] == 0xc0 | 0x1f);
-  assert(buf[1] == 154);
-  assert(buf[2] == 10);
+  assert(buf[1] == 0x9a);
+  assert(buf[2] == 0x0a);
   
   v = dec_integer(buf, sizeof(buf), 3, &rp);
   assert(v == 1337);
@@ -160,7 +160,7 @@ int main(int argc, char **argv){
   memset(buf, 0, sizeof(buf));
   r = enc_integer(buf, sizeof(buf), 42, 0);
   assert(r == 1);
-  assert(buf[0] == 42);
+  assert(buf[0] == 0x2a);
 
   v = dec_integer(buf, sizeof(buf), 0, &rp);
   assert(v == 42);
